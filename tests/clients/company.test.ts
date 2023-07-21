65457f12d7b38e49ff1a1af37c2ef77d5ce62fbb
Md Mahfuz Alam <mdmahfuzalam196@gmail.com>
2023-07-21 14:38:04 +0600

    some updated

--- tests/clients/company.test.ts
+++ tests/clients/company.test.ts
@@ -3,4 +3,5 @@ import * as data from "testData/login.cred.json"
 import { Page } from '@playwright/test';
 test("Validate company added successfully",async({page,loginPage,clientsPage}) =>{
   await page.goto("/login")
+  await loginPage.login(data.validusername,data.validpassword)
 })

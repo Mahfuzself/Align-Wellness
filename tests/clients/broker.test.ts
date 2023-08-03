bbc70acc7e4de97b69c26d2da39162d533537197
Md Mahfuz Alam <mdmahfuzalam196@gmail.com>
2023-08-03 12:48:12 +0600

    Added broker test file

---
+++ tests/clients/broker.test.ts
@@ -0,0 +1,6 @@
+import test, { expect } from "@fixtures/basepages";
+import * as data from "testData/login.cred.json"
+import { Page } from '@playwright/test';
+test("Validate add new broker",async({page,loginPage,clientsPage})=>{
+
+})
